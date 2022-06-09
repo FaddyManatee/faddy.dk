26273661f18f80e76ee6ba097fa682a323a7fd62
Thomas Morton <sc21tam@leeds.ac.uk>
2022-06-09 23:54:12 +0100

    Fixed bugs for random initial path (some cases remain where the algorithm does not work as intended)

--- javascript/404.js
+++ javascript/404.js
@@ -50,78 +50,92 @@ function RandomVector(rect) {
     var rndX = 0;
     var rndY = 0;
 
+    // Math.round(Math.random()); generates a random number 0-1 inclusive
     // Determine a general x direction (left, right)
-    var xDirection = Math.floor(Math.random() * 1);
+    var xDirection = Math.round(Math.random());
 
     // Determine a general y direction (up, down)
-    var yDirection = Math.floor(Math.random() * 1);
+    var yDirection = Math.round(Math.random());
 
     const limit = 500;
     var max = 0;
     var min = 0;
 
-    // Generate a coordinate outside of the canvas space using the general directions
-    if (xDirection == 0) {
+    // Generate a coordinate on the edge or outside of the canvas space using the general directions
+    if (yDirection == 0) {
         // Down
-        max = canvasHeight + limit;
-        min = canvasHeight;
+        max = (canvasHeight - 1) + limit;
+        min = canvasHeight - 1;
 
-        rndX = Math.floor(Math.random() * (max - min + 1) ) + min;
+        rndY = Math.floor(Math.random() * (max - min + 1) ) + min;
     }
     else {
         // Up
         max = 0;
         min = -limit;
 
-        rndX = Math.floor(Math.random() * (max - min + 1) ) + min;
+        rndY = Math.floor(Math.random() * (max - min + 1) ) + min;
     }
 
-    if (yDirection == 0) {
+    if (xDirection == 0) {
         // Right
-        max = canvasWidth + limit;
-        min = canvasWidth;
+        max = (canvasWidth - 1) + limit;
+        min = canvasWidth - 1;
 
-        rndY = Math.floor(Math.random() * (max - min + 1) ) + min;
+        rndX = Math.floor(Math.random() * (max - min + 1) ) + min;
     }
     else {
         // Left
         max = 0;
         min = -limit;
 
-        rndY = Math.floor(Math.random() * (max - min + 1) ) + min;
+        rndX = Math.floor(Math.random() * (max - min + 1) ) + min;
     }
 
     var coordinate = {x: rndX, y: rndY};
+    console.log(coordinate);
 
-    // Check each edge of the canvas for an intersection of the line between the rectangle position and the coordinate outside of the canvas
+    // Check each edge of the canvas for an intersection of the line between the rectangle position and the generated coordinate
     const canvasEdges = [{pos1: {x: 0, y: 0} , pos2: {x: canvasWidth - 1, y: 0}},
                          {pos1: {x: 0, y: canvasHeight - 1} , pos2: {x: canvasWidth - 1, y: canvasHeight - 1}},
                          {pos1: {x: 0, y: 0} , pos2: {x: 0, y: canvasHeight - 1}},
                          {pos1: {x: canvasWidth - 1, y: 0} , pos2: {x: canvasWidth - 1, y: canvasHeight - 1}}];
 
     var intersection;
+
+    // TEST
     console.log(canvasWidth);
     console.log(canvasHeight);
+
     for (var x = 0; x < canvasEdges.length; x++) {
         intersection = FindIntersection({pos1: {x: rect.x, y: rect.y}, pos2: coordinate}, canvasEdges[x]);
 
         // If the intersection is not within the canvas, continue
         if (intersection.x < 0 || intersection.x > canvasWidth - 1 || intersection.y < 0 || intersection.y > canvasHeight - 1)
+        {
+            // TEST
+            console.log(intersection);
             continue;
+        }
 
         // Break if there exists a valid intersection
         if (intersection.x == 0 || intersection.x == canvasWidth - 1 || intersection.y == 0 || intersection.y == canvasHeight - 1) {
-            // Tests
+            // TEST
             console.log(intersection);
+            g.lineTo(intersection.x, intersection.y);
             g.moveTo(intersection.x + 20, intersection.y);
             g.arc(intersection.x, intersection.y, 20, 0, 2 * Math.PI);
             g.stroke();
+            g.moveTo(rect.x, rect.y);
             break;
+        }
+        else {
+            // TEST
+            console.log(intersection);
         }
     }
 
     // Calculate the vector that brings the rectangle to the intersection and return it
-
 }
 
 
@@ -145,10 +159,14 @@ function FindIntersection(a, b) {
     if (Math.abs(gradientA) == Infinity) {
         solutionX = a.pos1.x;
         solutionY = (gradientB * solutionX) + interceptB;
+
+        return {x: solutionX, y: solutionY};
     }
     else if (Math.abs(gradientB) == Infinity) {
         solutionX = b.pos1.x;
-        solutionY = (gradientA * solutionX) + interceptA;  
+        solutionY = (gradientA * solutionX) + interceptA;
+
+        return {x: solutionX, y: solutionY};
     }
 
     // We now have enough information to solve the simultaneous equations a and b (both of the form y=mx+c)
